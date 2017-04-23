1739bb7e0bfd3d5c28120c66da8af394c0b5a35b
ribeirorvs <ribeiro.rvs@hotmail.com>
2017-04-23 16:14:23 -0300

    Atualização dos arquivos
    Inclusão de novas funções

--- Wave/test_wave.py
+++ Wave/test_wave.py
@@ -5,37 +5,61 @@ class TestWave(unittest.TestCase):
 
     def setUp(self):
         wave.remover_waves()
+        wave.zerar_turn()
 
     def test_criar_wave(self):
         wave.criar_wave()
 
         self.assertEqual(1, len(wave.waves))
+        
+
+    def test_criar_duas_wave(self):
+        wave.criar_wave()
+        wave.criar_wave()
+
+        self.assertEqual(2, len(wave.waves))
+        self.assertEqual(2, wave.turn)
+
 
     def test_definir_wave(self):
         wave.criar_wave()
-        wave.definir_wave("tristeza")
+        wave.definir_wave()
 
-        self.assertEqual(10, len(wave.waves["tristeza"]))
+        self.assertEqual(10, len(wave.waves["nojinho"]))
+
+    def test_definir_duas_wave(self):
+        wave.criar_wave()
+        wave.definir_wave()
+        wave.criar_wave()
+        wave.definir_wave()
+
+        self.assertEqual(12, len(wave.waves["nojinho"]))
+
 
     def test_diminuir_wave(self):
         wave.criar_wave()
-        wave.definir_wave("tristeza")
+        wave.definir_wave()
 
         wave.diminuir_wave(2)
 
-        self.assertEqual(9, len(wave.waves["tristeza"]))
+        self.assertEqual(9, len(wave.waves["nojinho"]))
         
 
     def test_get_wave(self):
         wave.criar_wave()
-        wave.definir_wave("tristeza")
+        wave.criar_wave()
+        wave.definir_wave()
+        
+        w = wave.get_wave()
 
-        w = wave.get_wave("tristeza", 2)
+    def test_criar_varias_wave(self):
+        for i in range(10):
+            wave.criar_wave()
 
-        self.assertEqual(5, w["hp_total"])
-        self.assertEqual(5, w["hp_atual"])
-        self.assertEqual(3, w["velocidade_padrao"])
-        self.assertEqual(3, w["velocidade_atual"])
+    def test_definir_varias_waves(self):
+        for i in range(5):
+            wave.criar_wave()
+
         
 
 if __name__ == '__main__':
--- Wave/wave.py
+++ Wave/wave.py
@@ -1,3 +1,6 @@
+import nojinho
+import medo
+import raiva
 import tristeza
 import random
 
@@ -6,35 +9,69 @@ new_wave = {}
 turn = 0
 
 def criar_wave():
+    global new_wave
+    remover_waves()
     if(turn == 0):
-        sentimento = "tristeza"
+        sentimento = "nojinho"
         qtde_bolinhas = 10
-        new_wave = {sentimento: qtde_bolinhas}
+        nw = {sentimento: qtde_bolinhas}
+        new_wave.update(nw)
+    elif(turn == 1):
+        sentimento = "nojinho"
+        qtde_bolinhas = 12
+        nw = {sentimento: qtde_bolinhas}
+        new_wave.update(nw)
+        sentimento = "medo"
+        qtde_bolinhas = 4
+        nw = {sentimento: qtde_bolinhas}
+        new_wave.update(nw)
+    elif(turn == 2):
+        sentimento = "medo"
+        qtde_bolinhas = 12
+        nw = {sentimento: qtde_bolinhas}
+        new_wave.update(nw)
+        sentimento = "raiva"
+        qtde_bolinhas = 4
+        nw = {sentimento: qtde_bolinhas}
+        new_wave.update(nw)
+    else:
+        sentimentos = _definir_total_bolinhas()
+        for i in sentimentos:
+            bolinhas = {i: sentimentos[i]}
+            new_wave.update(bolinhas)
+    _aumentar_turn()
     waves.update(new_wave)
 
 
-def definir_wave(sentimento):
+def definir_wave():
     global new_wave
     new_wave = {}
-    if (sentimento in waves):
-        for i in range(waves[sentimento]):
-            bolinha = {i:tristeza.criar_tristeza(1)}
-            new_wave.update(bolinha)
+    for sentimento in waves:
+        if(sentimento == "nojinho"):
+            for i in range(waves[sentimento]):
+                bolinha = {i:nojinho.criar_nojinho(turn)}
+                new_wave.update(bolinha)
+        elif(sentimento == "medo"):
+            for i in range(waves[sentimento]):
+                bolinha = {i:medo.criar_medo(turn)}
+                new_wave.update(bolinha)
+        elif(sentimento == "raiva"):
+            for i in range(waves[sentimento]):
+                bolinha = {i:raiva.criar_raiva(turn)}
+                new_wave.update(bolinha)
+        else:
+            for i in range(waves[sentimento]):
+                bolinha = {i:tristeza.criar_tristeza(turn)}
+                new_wave.update(bolinha)
         waves[sentimento] = new_wave
     
 
 def diminuir_wave(indice):
-    if(indice in waves["tristeza"]):
-        del(waves["tristeza"][indice])
+    if(indice in waves["nojinho"]):
+        del(waves["nojinho"][indice])
 
-def get_wave(sentimento, indice):
-    if(sentimento in waves):
-        if(indice in waves[sentimento]):
-            return waves[sentimento][indice]
-    return None
-
-def iniciar_wave():
-    pass
+def get_wave():
+    return waves
 
 
 def remover_waves():
@@ -43,4 +80,27 @@ def remover_waves():
     global turn
     waves = {}
     new_wave = {}
+
+def _aumentar_turn():
+    global turn
+    turn +=1
+
+def zerar_turn():
+    global turn
     turn = 0
+
+def _definir_qtde_sentimentos():
+    i = random.randint(1, 4)
+    opcoes = ("nojinho", "medo", "raiva", "tristeza")
+    sentimentos = []
+    for j in range(i):
+        sentimentos.append(opcoes[j])
+    return sentimentos
+
+def _definir_total_bolinhas():
+    s = _definir_qtde_sentimentos()
+    bolinhas = {}
+    for i in s:
+        b = {i: random.randint(10, turn*(random.randint(10, 20)))}
+        bolinhas.update(b)
+    return bolinhas
